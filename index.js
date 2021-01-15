72902b03eb4bde728980572ccfdf6e99fa2058a1
Zacharias Enochsson <zacharias.enochsson@gmail.com>
2021-01-15 11:34:15 +0100

    improve flattening and coverage

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
 import { h, text } from 'hyperapp'
 
-function flattenAll(input) {
-    const stack = [...input];
-    const res = [];
-    while(stack.length) {
-        const next = stack.pop();
-        if(Array.isArray(next)) {
-            stack.push(...next);
-        } else {
-            res.push(next);
-        }
-    }
-    return res.reverse();
+function flattenAll(input, result = []) {
+    input.forEach(x => {
+        if (Array.isArray(x)) flattenAll(x, result)
+        else result.push(x)
+    })
+    return result
 }
 
-/* Originally suggested by Jorge Bucaran on slack: */
 export default (type, props, ...children) =>
     typeof type === 'function'
-        ? type(props, children)
+        ? type(props, flattenAll(children))
         : h(
               type,
               props || {},
-              flattenAll(children)
-                  .map(any =>
-                      typeof any === 'string' || typeof any === 'number'
-                          ? text(any)
-                          : any
-                  )
+              flattenAll(children).map(child =>
+                  typeof child === 'string' || typeof child === 'number'
+                      ? text(child)
+                      : child
+              )
           )
--- test.js
+++ test.js
@@ -1,36 +1,56 @@
-import { h, text } from "hyperapp"
-import { t, deepEqual } from "twist"
-import jsx from "./index.js"
+import { h, text } from 'hyperapp'
+import { t, deepEqual } from 'twist'
+import jsx from './index.js'
+
+const simpleComponent = props => jsx('component', props)
+const containerComponent = (props, content) =>
+    jsx('container', props, [
+        jsx('preamble', {}, 'pre'),
+        content,
+        jsx('postamble', {}, 'post'),
+    ])
 
 export default [
-  t("hyperapp", [
-    t("simple jsx calls map to simple h calls", [
-      deepEqual(
-        h("zord", { foo: true }, []),
-        jsx("zord", { foo: true }, [])
-      ),
+    t('hyperapp', [
+        t('simple jsx calls map to simple h calls', [
+            deepEqual(
+                h('zord', { foo: true }, []),
+                jsx('zord', { foo: true }, [])
+            ),
+        ]),
+        t('strings become text nodes', [
+            deepEqual(h('p', {}, text('tenet')), jsx('p', {}, 'tenet')),
+        ]),
+        t('numbers become text nodes', [
+            deepEqual(h('p', {}, text(42)), jsx('p', {}, 42)),
+        ]),
+        t('nested arrays of children are flattened', [
+            deepEqual(
+                h('p', {}, [text('a'), text('b')]),
+                jsx('p', {}, ['a', ['b']])
+            ),
+            deepEqual(
+                h('p', {}, [text('a'), text('b'), text('c'), text('d')]),
+                jsx('p', {}, ['a', ['b', ['c'], 'd']])
+            ),
+        ]),
+        t('components work', [
+            deepEqual(h('component', {}), jsx(simpleComponent)),
+            deepEqual(h('component', { a: 1 }), jsx(simpleComponent, { a: 1 })),
+            deepEqual(
+                h('container', { a: 1 }, [
+                    h('preamble', {}, text('pre')),
+                    h('p', {}, text('child one')),
+                    h('p', {}, text('child two')),
+                    h('postamble', {}, text('post')),
+                ]),
+                jsx(
+                    containerComponent,
+                    { a: 1 },
+                    jsx('p', {}, 'child one'),
+                    jsx('p', {}, 'child two')
+                )
+            ),
+        ]),
     ]),
-    t("strings become text nodes", [
-      deepEqual(
-	    h("p", {}, text("tenet")),
-		jsx("p", {}, "tenet")
-	  ),
-    ]),
-    t("numbers become text nodes", [
-      deepEqual(
-	    h("p", {}, text(42)),
-		jsx("p", {}, 42)
-	  ),
-    ]),
-    t("nested arrays of children are flattened", [
-      deepEqual(
-	    h("p", {}, [text("a"), text("b")]),
-	    jsx("p", {}, ["a", ["b"]]),
-	  ),
-      deepEqual(
-	    h("p", {}, [text("a"), text("b"), text("c"), text("d")]),
-	    jsx("p", {}, ["a", ["b", ["c"], "d"]]),
-	  ),
-    ]),
-  ]),
 ]
